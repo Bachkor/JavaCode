905a78cbac4e4ac9dd6aef3ec90cc256c8c8a805
Muqthar <muqthar.hadi@nytimes.com>
2017-03-23 18:21:38 +0530

    Modifed the second print statement.

--- Demo11/src/Welcome.java
+++ Demo11/src/Welcome.java
@@ -4,7 +4,7 @@ public class Welcome
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to GitHub tool");
-		System.out.println("Welcome to GitHub Demo");
+		System.out.println("Welcome to Demo");
 	}
 
 }
